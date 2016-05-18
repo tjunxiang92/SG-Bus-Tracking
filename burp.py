99381c012acb68f96ba1000c1f95dfd4c9490640
Tan Jun Xiang <tjunxiang92@gmail.com>
2016-05-18 10:15:26 +0800

    First Commit

---
+++ burp.py
@@ -0,0 +1,32 @@
+import requests
+
+def code(text):
+	proto = 'https'
+	d = text
+
+	d = d.strip().split('\n')
+	header = d[0].split(' ')
+
+
+
+	# Crop out data
+	data = {}
+	for i in d[1:]:
+		if i == '':
+			break
+
+		h = i.split(': ')
+		data[h[0]] = h[1]
+
+	base_url = d[0]
+	data_type = header[0] # Type
+	url_params = header[1] # Url
+
+	url = "%s://%s%s" % (proto, data['Host'], url_params)
+
+	if header[0] == 'GET':
+		r = requests.get(url, headers=data)
+	else:
+		r = requests.post(url, headers=data, data=d[-1])
+
+	return r
---
+++ bus199.py
@@ -0,0 +1,63 @@
+from burp import *
+import time
+
+a = """
+POST /busbuzz/v1/bus_stops/search2 HTTP/1.1
+userid: 160200
+secret: ad1548d6791f37c15642a086966ab3f3d80919a6
+Content-Type: application/json; charset=UTF-8
+Content-Length: 74
+Host: by.originally.us
+Connection: close
+Accept-Encoding: gzip
+User-Agent: okhttp/2.2.0
+
+{"os_type":2,"os_version":23,"ver":"73.0","os":"android","keywords":"199"}
+"""
+
+r = code(a)
+b = r.json()
+b['extras'][0]['tab_name']
+
+c = """
+GET /busbuzz/v1/buses2/get_arrival/%s?os_type=2&os_version=23&ver=73.0&os=android HTTP/1.1
+userid: 160200
+secret: ad1548d6791f37c15642a086966ab3f3d80919a6
+Host: by.originally.us
+Connection: close
+Accept-Encoding: gzip
+User-Agent: okhttp/2.2.0
+
+""" 
+
+# Process Bus Stops
+bus_stops = b['extras'][0]['bus_stops']
+
+tracking_bus = 27051#bus_stops[1]['id'] # Bus Stop ID
+
+for i, bus_stop in enumerate(bus_stops):
+	bid = bus_stop['id']
+	if not tracking_bus == bid:
+		continue
+
+	while True:
+		cc = c % (bus_stop['id'])
+		timings = code(cc).json()
+		for timing in timings:
+			if not timing['service_number'] == 199:
+				continue
+
+			eta = timing['timing'][0]['etaText']
+			if eta == 0:
+				try:
+					tracking_bus = bus_stops[i + 1]['id'] # Next ID
+				except:
+					print 'Back to Interchange. Ending...'
+					exit()
+
+			print '%s: %s' % (bus_stop['bus_stop_name'], eta)
+
+		# Move next stop
+		if not tracking_bus == bid:
+			break
+		time.sleep(20)
